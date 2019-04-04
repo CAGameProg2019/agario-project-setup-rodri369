58d648e030d8bb1ccc48a18cfc7a4d899fb817cd
rodri369 <46567032+rodri369@users.noreply.github.com>
2019-04-04 08:25:35 -0400

    Update vector.js

--- vector.js
+++ vector.js
@@ -4,4 +4,29 @@ class Vector {
 
     }
 
+    addVector(vec){
+        this.x +=vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    //functions to write for HW
+    subVector(vec){
+        this.x -=vec.x;
+        this.y -= vec.y;
+        return this;
+
+    }
+
+    scale(s){
+        this.x *=s;
+        this.y *= s;
+        return this;
+
+    }
+    toString(){
+        return '<'+this.x + ',' +this.y + '>';
+    }
+
+
 }
