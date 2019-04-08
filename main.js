b3069113a06f56ee772ac45c3e0d3ae60ae10faa
rodri369 <46567032+rodri369@users.noreply.github.com>
2019-04-08 10:23:58 -0400

    Changes for April 8

--- main.js
+++ main.js
@@ -2,23 +2,35 @@ let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let pos = new Vector(60, 60);
-let vel = new Vector(.5,.2);
+let foods = [];
+let colors = [
+    'blue',
+    'orange',
+    'red',
+    'green',
+    'purple',
+    'teal',
+];
 
+function randomColor() {
+    let index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
 function init() {
+    for(i=0; i<100;i++){
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        let color = randomColor();
+        foods.push(new Food(x,y,20, color));
+    }
     update();
 }
 
 function update() {
-    c.clearRect(0,0,canvas.width, canvas.height);
-
-    pos.addVector(vel);
-
-    c.beginPath();
-    c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false);
-    c.closePath();
-    c.stroke();
-
+    c.clearRect(0,0,canvas.width/2, canvas.height/2);
+    for(i=0;i<100;i++){
+        foods[i].draw(c);
+    }
     requestAnimationFrame(update);
 }
 
