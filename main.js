a012c9016af033ee1e5412061ca2c6712d66a218
rodri369 <46567032+rodri369@users.noreply.github.com>
2019-04-11 09:11:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let player;
+let mpos;
+let velVetor = new Vector(0,0_;
+)
 let foods = [];
 let colors = [
     'blue',
@@ -11,6 +16,11 @@ let colors = [
     'purple',
     'teal',
 ];
+
+function calcVel(){
+    velVector.x = (mpos.x-player.x)/10;
+    velVector.y = (mpos.y-player.y)/10;
+}
 
 function randomColor() {
     let index = Math.floor(Math.random() * colors.length);
@@ -31,9 +41,17 @@ function update() {
     for(i=0;i<100;i++){
         foods[i].draw(c);
     }
+    calcVel();
+    player.addVector(velVector);
+    player.draw(c);
     requestAnimationFrame(update);
 }
 
 window.addEventListener('load', function(event) {
     init();
+
+window.addEventListener('mousemove', function(event){
+    mpos.x = event.clientX - canvas.offsetLeft;
+    mpos.y = event.clientY - canvas.offsetTop;
+})
 });
